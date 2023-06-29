eddef8cf1a6810835d5ad4104315a5783faa6ef8
maurinei-miranda <maurinei.develop@gmail.com>
2023-06-29 19:52:11 -0300

    Add GetCustomer use case

---
+++ src/main/java/com/custom/marketplace/domain/usecases/GetCustomer.java
@@ -0,0 +1,20 @@
+package com.custom.marketplace.domain.usecases;
+
+import com.custom.marketplace.domain.entities.Customer;
+import com.custom.marketplace.domain.repositories.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetCustomer {
+  final CustomerRepository customerRepository;
+
+
+  public GetCustomer(CustomerRepository customerRepository) {
+    this.customerRepository = customerRepository;
+  }
+
+  public Customer call(Long id){
+    Customer customer = customerRepository.getCustomer(id);
+    return customer;
+  }
+}
