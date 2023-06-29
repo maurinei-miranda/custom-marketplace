479525f012b12d141409ea4183c64fa1a5068168
maurinei-miranda <maurinei.develop@gmail.com>
2023-06-29 19:51:56 -0300

    Add CustomerMapper configuration bean

---
+++ src/main/java/com/custom/marketplace/core/configurations/RowMapperConfiguration.java
@@ -0,0 +1,16 @@
+package com.custom.marketplace.core.configurations;
+
+
+import com.custom.marketplace.data.mappers.impl.CustomerMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RowMapperConfiguration {
+
+  @Bean
+  public CustomerMapper customerRowMapper(){
+    CustomerMapper customerMapper = new CustomerMapper();
+    return customerMapper;
+  }
+}
